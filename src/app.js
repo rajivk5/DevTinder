ed63f47b3ee2cbc83f31a42c5b99196b14ae7a5f
Rajiv Kumar <rajivr021@gmail.com>
2026-09-25 00:14:27 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -5,10 +5,28 @@ const app = express();
 const { middleware1, middleware2, middleware3 } = require('./middlewares/auth');
 const userRouter = require('./routs/userRoutes');
 
-app.use(middleware1);
-app.use(middleware2);
-app.use(middleware3);
-app.get('/', userRouter)
+// app.use(middleware1);
+// app.use(middleware2);
+// app.use(middleware3);
+// app.get('/', userRouter)
+
+
+app.get("/test", (req, res, next) => {
+    console.log("Route");
+    next(new Error("Something broke"));
+});
+
+app.use((err, req, res, next) => {
+    console.log("Error Handler 1");
+
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console.log("Error Handler 2");
+
+    res.status(500).send("Failed");
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
